971520da10d35e314f9bbb18bf94409e72261c39
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-05-20 21:11:40 +0530

    model and database connection part done

---
+++ server/controllers/userController.js
@@ -0,0 +1,34 @@
+import User from "../models/User.js"
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+//SignUp a new user
+export const signup=async()=>{
+    const {fullName,email,password,bio}=req.body;
+
+    try{
+        if(!fullName || !email || !password || !bio){
+            return res.json({success:false,message:"Missing Details"})
+        }
+        const user=await User.findOne({email});
+
+        if(user){
+            return res.json({success:false,message:"Account already exists"})
+        }
+
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser=await User.create({
+            fullName,email,password:hashedPassword,bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success:true, userData:newUser, token,message:"Account created successfully"})
+
+    }catch(error){
+        console.log(error.message);
+            res.json({success:false,message:error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors"
 import http from "http"
+import { connectDB } from "./lib/db.js";
 
 //create express app and http server
 
 const app=express();
-const server=http.createServer(app)
+const server= http.createServer(app)
 
 //middleware setup
 app.use(express.json({limit:"4mb"}));
@@ -14,5 +15,8 @@ app.use(cors());
 
 app.use("/api/status",(req,res)=> res.send("Server is live"));
 
+//Connect to mongodb
+await connectDB();
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>console.log("SErver is running on PORT: " + PORT));
+server.listen(PORT,()=>console.log("Server is running on PORT: " + PORT));
