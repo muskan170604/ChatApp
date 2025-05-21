76072789b78a592ee499910cabda617e232d97c4
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-05-21 12:34:51 +0530

    middleware part done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -54,3 +54,8 @@ export const login=async(req,res)=>{
             res.json({success:false,message:error.message})
     }
 }
+
+//Controller to check if user is authenticated
+export const checkAuth=(req,res)=>{
+    res.json({success:true,user:req.user});
+}
