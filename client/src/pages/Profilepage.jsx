2d51c7b1aa52cc5cac24e566e35d02cf4b1f2b9a
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-05-28 15:31:06 +0530

    chatContextPart done

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -48,7 +48,7 @@ const ProfilePage = () => {
                 <textarea onChange={(e)=>setBio(e.target.value)} value={bio} placeholder='Write profile bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
                 <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
             </form>
-                <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full' }`} src={assets.logo_icon} alt="" />
+                <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full' }`} src={authUser?.profilePic|| assets.logo_icon} alt="" />
         </div>
     </div>
     )
